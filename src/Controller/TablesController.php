3f3a8f3f31f30599c112814fc20616f5899b6c29
oussemaghazouani <oussamaghazouani95@gmail.com>
2024-11-23 22:12:03 +0100

    integration template+crud user

---
+++ src/Controller/TablesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class TablesController extends AbstractController
+{
+    #[Route('/tables', name: 'app_tables')]
+    public function index(): Response
+    {
+        return $this->render('tables/index.html.twig', [
+            'controller_name' => 'TablesController',
+        ]);
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $EMAIL = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $motdepasse = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getEMAIL(): ?string
+    {
+        return $this->EMAIL;
+    }
+
+    public function setEMAIL(string $EMAIL): static
+    {
+        $this->EMAIL = $EMAIL;
+
+        return $this;
+    }
+
+    public function getMotdepasse(): ?string
+    {
+        return $this->motdepasse;
+    }
+
+    public function setMotdepasse(string $motdepasse): static
+    {
+        $this->motdepasse = $motdepasse;
+
+        return $this;
+    }
+}
