6c5473d5bf03b14f984fd1ed371894bfa0cf431b
oussemaghazouani <oussamaghazouani95@gmail.com>
2024-12-11 13:04:27 +0100

    oussama

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,11 +9,37 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/user')]
 final class UserController extends AbstractController
 {
+    #[Route('/signup', name: 'app_signup', methods: ['GET', 'POST'])]
+    public function signup(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $isAdmin = false; // Les clients ne voient pas le champ rôle
+        $user = new User();
+        $user->setRole("ROLE_CLIENT"); // Rôle par défaut pour les clients
+
+        $form = $this->createForm(UserType::class, $user, [
+            'is_admin' => $isAdmin, // Désactiver la sélection de rôle
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // Rediriger vers la liste des utilisateurs ou une autre page
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('user/signup.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
     #[Route(name: 'app_user_index', methods: ['GET'])]
     public function index(UserRepository $userRepository): Response
     {
@@ -25,25 +51,26 @@ final class UserController extends AbstractController
     #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $isAdmin = true; // Les administrateurs peuvent définir le rôle
         $user = new User();
-        $form = $this->createForm(UserType::class, $user);
+
+        $form = $this->createForm(UserType::class, $user, [
+            'is_admin' => $isAdmin, // Activer la sélection de rôle
+        ]);
         $form->handleRequest($request);
-        
-    
+
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->persist($user);
             $entityManager->flush();
-    
+
             return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
         }
-    
-        // Si le formulaire n'est pas soumis ou invalide, afficher la vue du formulaire
+
         return $this->render('user/new.html.twig', [
             'user' => $user,
             'form' => $form->createView(),
         ]);
     }
-  
 
     #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
     public function show(User $user): Response
@@ -56,8 +83,9 @@ final class UserController extends AbstractController
     #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
     {
-        
-        $form = $this->createForm(UserType::class, $user);
+        $form = $this->createForm(UserType::class, $user, [
+            'is_admin' => true, // Activer la sélection de rôle
+        ]);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -68,20 +96,18 @@ final class UserController extends AbstractController
 
         return $this->render('user/edit.html.twig', [
             'user' => $user,
-            'form' => $form,
+            'form' => $form->createView(),
         ]);
     }
+
     #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
     public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->getPayload()->getString('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->request->get('_token'))) {
             $entityManager->remove($user);
             $entityManager->flush();
         }
 
         return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
     }
-    
-
-    
 }
